807d76d1a3ddb986d10ca03b4b745ae1edc5ea75
shahushinde <shahushinde50@gmail.com>
2022-11-14 09:54:10 +0530

    initial commit

---
+++ src/main/java/com/DSA_Bank/DsaBankApplication.java
@@ -0,0 +1,13 @@
+package com.DSA_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DSA_Bank/DsaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DSA_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
